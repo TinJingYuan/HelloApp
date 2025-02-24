274f9fc4d9f4135a2ece84c93a201bd25af6f99c
txk941115 <txk941115@gmail.com>
2025-02-24 10:43:53 +0800

    End1

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -1,14 +1,15 @@
-﻿namespace HelloApp
+﻿using System;
+namespace HelloApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
             int a, b;
             string[] input = Console.ReadLine().Split();
             a = int.Parse(input[0]);
             b = int.Parse(input[1]);
+            Console.WriteLine($"{a}+{b}={a + b}");
         }
     }
 }
